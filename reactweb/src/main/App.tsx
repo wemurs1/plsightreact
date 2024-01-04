5d9a6c53e7f33ba01c2272c426a1dcd7314e67a9
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-04 16:31:27 +0000

    set up routing

--- reactweb/src/main/App.tsx
+++ reactweb/src/main/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HouseList from '../house/HouseList';
 import './App.css';
 import Header from './Header';
 
 function App() {
   return (
-    <div className='container'>
-      <Header subtitle='Providing houses all over the world'></Header>
-      <HouseList />
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <Header subtitle='Providing houses all over the world'></Header>
+        <Routes>
+          <Route path='/' element={<HouseList />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
