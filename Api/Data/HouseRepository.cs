793c8c6f9220f3129b3fc88177c27dfea305f504
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-05 09:29:52 +0000

    complete CRUD of Houses

--- Api/Data/HouseRepository.cs
+++ Api/Data/HouseRepository.cs
@@ -7,6 +7,9 @@ public interface IHouseRepository
 {
     Task<List<HouseDto>> GetAll();
     Task<HouseDetailsDto?> Get(int id);
+    Task<HouseDetailsDto> Add(HouseDetailsDto dto);
+    Task<HouseDetailsDto> Update(HouseDetailsDto dto);
+    Task Delete(int id);
 }
 
 public class HouseRepository : IHouseRepository
@@ -28,6 +31,47 @@ public class HouseRepository : IHouseRepository
         var e = await context.Houses.SingleOrDefaultAsync(h => h.Id == id);
         if (e == null) return null;
 
-        return new HouseDetailsDto(e.Id, e.Address, e.Country, e.Price, e.Description, e.Photo);
+        return EntityToDetailDto(e);
+    }
+
+    public async Task<HouseDetailsDto> Add(HouseDetailsDto dto)
+    {
+        var entity = new HouseEntity();
+        DtoToEntity(dto, entity);
+        context.Houses.Add(entity);
+        await context.SaveChangesAsync();
+        return EntityToDetailDto(entity);
+    }
+
+    public async Task<HouseDetailsDto> Update(HouseDetailsDto dto)
+    {
+        var entity = await context.Houses.FindAsync(dto.Id);
+        if (entity == null) throw new ArgumentException($"Error updating house {dto.Id}");
+        DtoToEntity(dto, entity);
+        context.Entry(entity).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+        return EntityToDetailDto(entity);
+    }
+
+    public async Task Delete(int id)
+    {
+        var entity = await context.Houses.FindAsync(id);
+        if (entity == null) throw new ArgumentException($"Error deleting house {id}");
+        context.Houses.Remove(entity);
+        await context.SaveChangesAsync();
+    }
+
+    private static void DtoToEntity(HouseDetailsDto dto, HouseEntity e)
+    {
+        e.Address = dto.Address;
+        e.Country = dto.Country;
+        e.Description = dto.Description;
+        e.Price = dto.Price;
+        e.Photo = dto.Photo;
+    }
+
+    private static HouseDetailsDto EntityToDetailDto(HouseEntity e)
+    {
+        return new HouseDetailsDto(e.Id, e.Address, e.Country, e.Price, e.Description, e.Country);
     }
 }
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,6 @@
 using Api.Data;
 using Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +27,7 @@ app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAn
 app.UseHttpsRedirection();
 
 app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll()).Produces<HouseDto[]>(StatusCodes.Status200OK);
+
 app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
 {
     var house = await repo.Get(houseId);
@@ -33,5 +35,25 @@ app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
 
     return Results.Ok(house);
 }).ProducesProblem(404).Produces<HouseDetailsDto>(StatusCodes.Status200OK);
+
+app.MapPost("/houses", async ([FromBody] HouseDetailsDto dto, IHouseRepository repo) =>
+{
+    var newHouse = await repo.Add(dto);
+    return Results.Created($"/house/{newHouse.Id}", newHouse);
+}).Produces<HouseDetailsDto>(StatusCodes.Status201Created);
+
+app.MapPut("/houses", async ([FromBody] HouseDetailsDto dto, IHouseRepository repo) =>
+{
+    if (await repo.Get(dto.Id) == null) return Results.Problem($"House {dto.Id} not found", statusCode: 404);
+    var updatedHouse = await repo.Update(dto);
+    return Results.Ok(updatedHouse);
+}).ProducesProblem(404).Produces<HouseDetailsDto>(StatusCodes.Status200OK);
+
+app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
+{
+    if (await repo.Get(houseId) == null) return Results.Problem($"House {houseId} not found", statusCode: 404);
+    await repo.Delete(houseId);
+    return Results.Ok();
+}).ProducesProblem(404).Produces(StatusCodes.Status200OK);
 
 app.Run();
